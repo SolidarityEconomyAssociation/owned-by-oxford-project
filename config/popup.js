020d15146dc1f690365a6bb9b1e03d9e6260eb2d
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-11-08 17:08:29 +0000

    config/popup.js - customise this for the current datasets
    Use the common fields. Omit any vocabs, as only Solidarity Oxford
    seems to use them.

--- config/popup.js
+++ config/popup.js
@@ -43,77 +43,18 @@ function getWebsite(initiative) {
   return '';
 }
 
-function getBMT(initiative, bmtVocab) {
-  if (initiative.baseMembershipType) {
-    return `${bmtVocab.title}: ${bmtVocab.terms[initiative.baseMembershipType]}`;
-  }
-  
-  return `${bmtVocab.title}: Others`;
-}
-
-function getOrgStructure(initiative, osVocab, acVocab, qfVocab) {
-  if (!initiative.qualifier && initiative.orgStructure && initiative.orgStructure.length > 0) {
-    const terms = initiative.orgStructure.map(id => osVocab.terms[id]).join(", ");
-    return `${osVocab.title}: ${terms}`;
-  }
-
-  if (!initiative.qualifier && initiative.regorg) {
-    if (!osVocab.terms[initiative.regorg])
-      console.error(`Unknown ${osVocab.title} vocab term ID: ${initiative.regorg}`);
-    return `${osVocab.title}: ${osVocab.terms[initiative.regorg]}`;
-  }
-  
-  if (initiative.qualifier) {
-    if (!qfVocab.terms[initiative.qualifier])
-      console.error(`Unknown ${qfVocab.title} vocab term ID: ${initiative.qualifier}`);
-    return `${osVocab.title}: ${qfVocab.terms[initiative.qualifier]}`;
-  }
-
-  return '';
-}
-
-function getPrimaryActivity(initiative, acVocab) {
-  if (initiative.primaryActivity && initiative.primaryActivity != "") {
-    return `${acVocab.title}: ${acVocab.terms[initiative.primaryActivity]}`;
-  }
-  
-  return '';
-}
-
-function getSecondaryActivities(initiative, acVocab, osVocab) {
-  const title = "Secondary Activities"; // FIXME not yet translated!
-  
-  if (initiative.activities && initiative.activities.length > 0) {
-    const term = initiative.activities.map(id => acVocab.terms[id]).join(", ");
-    return `${title}: ${term}`;
-  }
-  
-  if (initiative.activity) {
-    return `${title}: ${osVocab.terms[initiative.activity]}`;
-  }
-
-  return '';
-}
-
-function getEmail(initiative) {
-  // Not all orgs have an email
-  if (initiative.email)
-    return `<a class="fa fa-at" href="mailto:${initiative.email}" target="_blank" ></a>`;
-  return "";
+function linkTo(url, options) {
+  return `<a class="${options.class}" href="${url}" target="_blank" ></a>`;
 }
 
 function getFacebook(initiative) {
-  // not all have a facebook
-  if (initiative.facebook)
-    return `<a class="fab fa-facebook" href="https://facebook.com/${initiative.facebook}" target="_blank" ></a>`;
-  return "";
+  return initiative.facebook? linkTo(`https://facebook.com/${initiative.facebook}`,
+                                     {class: "fab fa-facebook"}) : '';
 }  
 
 function getTwitter(initiative) {
-  // not all have twitter
-  if (initiative.twitter)
-    return `<a class="fab fa-twitter" href="https://twitter.com/${initiative.twitter}" target="_blank" ></a>`;
-  return '';
+  return initiative.twitter? linkTo(`https://twitter.com/${initiative.twitter}`,
+                                    {class: "fab fa-twitter"}) : '';
 }
 
 function getPopup(initiative, sse_initiatives) {
@@ -124,14 +65,11 @@ function getPopup(initiative, sse_initiatives) {
   
   const values = sse_initiatives.getLocalisedVocabs();
   const labels = sse_initiatives.getFunctionalLabels();
+
   let popupHTML = `
     <div class="sea-initiative-details">
       <h2 class="sea-initiative-name">${initiative.name}</h2>
       ${getWebsite(initiative)}
-      <h4 class="sea-initiative-org-structure">${getOrgStructure(initiative, values["os:"], values["aci:"], values["qf:"])}</h4>
-      <h4 class="sea-initiative-org-typology">${getBMT(initiative, values["bmt:"])}</h4>
-      <h4 class="sea-initiative-economic-activity">${getPrimaryActivity(initiative, values["aci:"])}</h4>
-      <h5 class="sea-initiative-secondary-activity">${getSecondaryActivities(initiative, values["aci:"], values["os:"])}</h5>
       <p>${initiative.desc || ''}</p>
     </div>
     
@@ -140,7 +78,6 @@ function getPopup(initiative, sse_initiatives) {
       ${getAddress(initiative, getTerm)}
       
       <div class="sea-initiative-links">
-        ${getEmail(initiative)}
         ${getFacebook(initiative)}
         ${getTwitter(initiative)}
       </div>
